7e7714c847e6b220f09f16e4edcd4cd85bc2695e
Wendel Soares <wendel.soares.dev@gmail.com>
2022-04-06 12:03:31 -0300

    SellersController

---
+++ SalesWebMvc/Views/Sellers/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SalesWebMvc.Views.Sellers
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
